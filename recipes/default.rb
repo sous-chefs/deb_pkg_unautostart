f206e531f30aa9531b12dbae0413a42fc976c5ae
Tim Smith <tsmith@chef.io>
2016-08-05 16:50:51 -0700

    Resolve cookstyle warnings
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,6 +3,4 @@ f = file '/usr/sbin/policy-rc.d' do
   content("#!/bin/sh\nexit 101\n")
 end
 
-if(node[:deb_pkg_unautostart][:compiletime])
-  f.run_action(:create)
-end
+f.run_action(:create) if node[:deb_pkg_unautostart][:compiletime]
