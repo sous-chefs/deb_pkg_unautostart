d9cf9561bfd7a66a88e955515559576ea90a0a00
Tim Smith <tsmith@chef.io>
2016-08-05 16:46:36 -0700

    Add chef as the maintainer and add more metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,14 @@
 name 'deb_pkg_unautostart'
 version '0.1.0'
-maintainer 'Chris Roberts'
-maintainer_email 'chrisroberts.code@gmail.com'
-description 'Stop deb packages from starting processes'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
+description 'Stop deb packages from starting services'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+supports 'ubuntu'
+supports 'debian'
+
+source_url 'https://github.com/chef-cookbooks/deb_pkg_unautostart' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/deb_pkg_unautostart/issues' if respond_to?(:issues_url)
+
+chef_version '>= 11' if respond_to?(:chef_version)
