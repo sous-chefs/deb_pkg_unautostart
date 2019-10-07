deabdfe77af1852fe5fab64207d1cae2dc5410ec
Tim Smith <tsmith84@gmail.com>
2019-10-07 16:27:23 -0700

    Resolve cookstyle 5.8 warnings
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Stop deb packages from starting services'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.0'
 
 supports 'ubuntu'
